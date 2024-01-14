be8c485e2e03a1422477f006cdab5662bbe5fc8c
jdev <j.dev@iitg.ac.in>
2024-01-14 16:27:09 +0530

    Recommended Exams

--- intellifill/src/components/Card.js
+++ intellifill/src/components/Card.js
@@ -7,35 +7,39 @@ function Card({props}) {
   return (
     <div className='Card'>
         <div className='top'>
-            <div className='icon'>
-                <img src={props.icon} alt="" srcset="" />
+            <div className='icon' >
+                <img width={`100%`} src={props.icon} alt="" srcset="" />
             </div>
             <div className='detail'>
                 <div className='name'>
                     {props.name}
                 </div>
-                <div className='org'>
+                <div className='org text2'>
                     {props.org}
                 </div>
             </div>
             <div className='save'>
-                
+                <img src="/bookmark.png" alt="" />
             </div>
         </div>
         <div className='bottom'>
             <div className='Participants'>
-                {`More than ${props.participants}`}
+                <img src='/crowd.png' width={`30px`} style={{marginRight:`10px`}}/>
+                <div>
+                  {`More than ${props.participants}`}
+                </div>
             </div>
             <div className='fees'>
-                {`₹ ${props.fees}`}
+                <img src='/wallet.png' width={`30px`} style={{marginRight:`10px`}}/>
+                <div>{`₹ ${props.fees}`}</div>
             </div>
             <div className='tags'>
-                {props.tag && props.tag.map((e)=>{
-                    return <div className='tag'>e</div>
+                {props.tags && props.tags.map((e)=>{
+                    return <div className='tag'>{e}</div>
                 })}
             </div>
             <div className='viewDetail'>
-                <a href="http://" target="_blank" rel="noopener noreferrer"></a>
+                <a href="http://" target="_blank" rel="noopener noreferrer">View Details  <div className='Arrow'>&gt;</div></a>
             </div>
         </div>
     </div>
