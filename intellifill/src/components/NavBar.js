bd7064c53c0e42ab85af8b2c213aef192af35265
jdev <j.dev@iitg.ac.in>
2024-01-14 09:47:42 +0530

    NavBar

---
+++ intellifill/src/components/NavBar.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import '../styles/NavBar.css'
+// import dp from './dp.jpeg'
+
+function NavBar() {
+  return (
+    <div className='NavBar'>
+        <div className='NavBar_icon NavBar_ele'>FormEase</div>
+        <div className='NavBar_link NavBar_ele NavBar_Home'>Home</div>
+        <div className='NavBar_link NavBar_ele NavBar_Result'>Result</div>
+        <div className='NavBar_link NavBar_ele NavBar_FAQ'>FAQ</div>
+        <div className='NavBar_link NavBar_ele NavBar_AboutUs'>About Us</div>
+        <div className='NavBar_link NavBar_ele NavBar_ContactUs'>Contact Us</div>
+        <div className='NavBar_search NavBar_ele'>
+          <input style={{backgroundImage: `url("/search.png")`}} className='NavBar_search_input' placeholder='Search Exams Here' type="search" name="" id="" />
+          </div>
+        <div className=' NavBar_ele'><img className='NavBar_notif' src="/notif.png" alt="" srcset="" /></div>
+        <div className='NavBar_ele'><img className='NavBar_dp' src='./dp.jpeg' alt="" srcset="" /></div>
+        <br />
+    </div>
+  )
+}
+
+export default NavBar
